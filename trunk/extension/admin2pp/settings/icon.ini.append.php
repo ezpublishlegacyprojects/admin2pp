dbf51436499d2fea2f247294f6559895d6b0aaed
Damien Pobel <damien@pobel.fr>
2010-02-20 12:52:06 +0000

    Added an optimized icons theme and added expires settings
    git-svn-id: http://svn.projects.ez.no/admin2pp@20 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

---
+++ trunk/extension/admin2pp/settings/icon.ini.append.php
@@ -0,0 +1,16 @@
+<?php /*
+#
+# $Id$
+# $HeadURL$
+#
+
+[IconSettings]
+Theme=crystal-admin-opt43
+Repository=extension/admin2pp/share/icons
+
+[FlagIcons]
+Repository=extension/admin2pp/share/icons
+Theme=flags-opt43
+IconFormat=png
+
+*/ ?>
