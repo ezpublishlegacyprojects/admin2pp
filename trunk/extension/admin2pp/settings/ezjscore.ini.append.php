8d31204e7ecce9e8b2d5e7b3306ecccb6c17ceb3
Damien Pobel <damien@pobel.fr>
2010-02-22 21:05:43 +0000

    Configure ezjscore for Expires headers
    git-svn-id: http://svn.projects.ez.no/admin2pp@33 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

--- trunk/extension/admin2pp/settings/ezjscore.ini.append.php
+++ trunk/extension/admin2pp/settings/ezjscore.ini.append.php
@@ -8,6 +8,10 @@
 ExternalScripts[jqueryui]=http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js
 LocalScripts[jqueryui]=jquery-ui-1.7.2.min.js
 
+
+[Packer]
+AppendLastModifiedTime=enabled
+
 [ezjscServer_admin2pp]
 Class=admin2ppFunctions
 File=extension/admin2pp/classes/admin2ppfunctions.php
