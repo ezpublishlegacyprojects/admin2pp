2478a5b635e589923b78de499a19133934621c5f
Damien Pobel <damien@pobel.fr>
2010-02-26 22:01:53 +0000

    Added missing javascript files to BackendJavaScriptList setting so that packed JS file is the same on most pages
    git-svn-id: http://svn.projects.ez.no/admin2pp@37 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

--- trunk/extension/admin2pp/settings/design.ini.append.php
+++ trunk/extension/admin2pp/settings/design.ini.append.php
@@ -12,4 +12,12 @@ DesignExtensions[]=admin2pp
 BackendCSSFileList[]=ez43/jquery-ui-1.7.2.custom.css
 BackendCSSFileList[]=admin2pp.css
 
+
+[JavaScriptSettings]
+BackendJavaScriptList[]=ezjsc::yui3
+BackendJavaScriptList[]=ezjsc::yui3io
+BackendJavaScriptList[]=node_tabs.js
+BackendJavaScriptList[]=ezajaxsubitems_sortdd.js
+BackendJavaScriptList[]=ezajaxrelations_jquery.js
+
 */ ?>
