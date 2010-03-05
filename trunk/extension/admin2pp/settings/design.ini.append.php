3723bcd5fc37b986587b9aa2ca546b5254099946
Damien Pobel <damien@pobel.fr>
2010-03-05 16:55:55 +0000

    Added a new CSS file for design improvements not related to widget to make it easier to disable
    git-svn-id: http://svn.projects.ez.no/admin2pp@57 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

--- trunk/extension/admin2pp/settings/design.ini.append.php
+++ trunk/extension/admin2pp/settings/design.ini.append.php
@@ -12,6 +12,13 @@ DesignExtensions[]=admin2pp
 BackendCSSFileList[]=ez43/jquery-ui-1.7.2.custom.css
 BackendCSSFileList[]=admin2pp.css
 
+# stylesheet to improve some default styles like
+#   * yellow background in input/textarea when focused
+#   * round corner on all input/textarea fields
+#   * style on context menu
+# comment the following if you don't like it
+BackendCSSFileList[]=admin2pp_improved_default.css
+
 
 [JavaScriptSettings]
 BackendJavaScriptList[]=ezjsc::yui3
