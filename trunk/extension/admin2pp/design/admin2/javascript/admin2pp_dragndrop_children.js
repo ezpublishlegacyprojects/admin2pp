14c17a6936d0b519be3fc9b30b8fbd4b08d2c023
Damien Pobel <damien@pobel.fr>
2010-03-13 18:47:22 +0000

    Replaced YUI3 code for drag'n drop with jQueryUI
    git-svn-id: http://svn.projects.ez.no/admin2pp@78 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

---
+++ trunk/extension/admin2pp/design/admin2/javascript/admin2pp_dragndrop_children.js
@@ -0,0 +1,85 @@
+/**
+ * $Id$
+ * $HeadURL$
+ *
+ */
+
+function admin2ppDragNDropChildren()
+{
+    this.sortOrder = 1;
+}
+
+admin2ppDragNDropChildren.PRIORITY_OFFSET = 2;
+admin2ppDragNDropChildren.TABLE_ID = 'ezasi-subitems-list';
+
+admin2ppDragNDropChildren.prototype =
+{
+    init:function()
+         {
+             var instance = this;
+             this.sortOrder = jQuery( '#ezasi-sort-order' ).val() === '0' ? 0 : -1;
+             jQuery('#' + admin2ppDragNDropChildren.TABLE_ID + ' tbody').sortable( { placeholder: 'ui-state-highlight',
+                                                                                     axis:'y',
+                                                                                     opacity:0.8,
+                                                                                     items:'tr',
+                                                                                     cursor:'move',
+                                                                                     forceHelperSize:true,
+                                                                                     forcePlaceholderSize:true,
+                                                                                     change:function( evt, ui )
+                                                                                     {
+                                                                                         instance.fixPlaceHolder( ui );
+                                                                                     },
+                                                                                     stop:function( evt, ui )
+                                                                                     {
+                                                                                         instance.fixBackgrounds(); 
+                                                                                         instance.setPriorities();
+                                                                                         instance.hightlightUpdateButton();
+                                                                                     }
+                                                                                   } )
+                                                                        .css( 'cursor', 'move' );
+             jQuery('#' + admin2ppDragNDropChildren.TABLE_ID + ' tbody').disableSelection();
+             jQuery('#' + admin2ppDragNDropChildren.TABLE_ID + ' input.ezasi-priority-input').change(function()
+                                                                                                     {
+                                                                                                        instance.hightlightUpdateButton();
+                                                                                                     });
+     },
+
+    hightlightUpdateButton:function()
+                           {
+                               jQuery('#ezasi-update-priority').addClass('defaultbutton');
+                           },
+
+    setPriorities:function()
+                  {
+                      var inputs = jQuery( '#' + admin2ppDragNDropChildren.TABLE_ID + ' input.ezasi-priority-input' );
+                      var starts = inputs.size() * this.sortOrder * admin2ppDragNDropChildren.PRIORITY_OFFSET;
+                      inputs.each(function()
+                                  {
+                                      jQuery( this ).val( starts ); 
+                                      starts = starts + admin2ppDragNDropChildren.PRIORITY_OFFSET;
+                                  });
+                  },
+
+    fixPlaceHolder:function( ui )
+                   {
+                       var pl = ui.placeholder;
+                       pl.html( '<td colspan="' + ui.item.children().size() + '">&nbsp;</td>' ); 
+                   },
+    fixBackgrounds:function()
+                   {
+                       var cssClass = 'bglight';
+                       jQuery('#' + admin2ppDragNDropChildren.TABLE_ID + ' tbody tr').each(function()
+                                                                                           {
+                                                                                               jQuery( this ).attr( 'class', cssClass );
+                                                                                               if ( cssClass == 'bglight' )
+                                                                                               {
+                                                                                                   cssClass = 'bgdark';
+                                                                                               }
+                                                                                               else
+                                                                                               {
+                                                                                                   cssClass = 'bglight';
+                                                                                               }
+                                                                                           });
+                   }
+
+}
