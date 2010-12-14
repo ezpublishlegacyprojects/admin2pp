7689d3ac33633a65388827cce85f9b7b8244962c
Damien Pobel <damien@pobel.fr>
2010-12-14 14:37:20 +0000

    Added extension.xml and update ezinfo.php to reflect upgrade to jQueryUI 1.8.6 and to prepare future release
    git-svn-id: http://svn.projects.ez.no/admin2pp@125 7d5823d8-de58-4d2a-bab8-4b9ce82eb87f

--- trunk/extension/admin2pp/ezinfo.php
+++ trunk/extension/admin2pp/ezinfo.php
@@ -29,11 +29,11 @@ class admin2ppInfo
     static function info()
     {
         return array( 'Name'      => '<a href="http://projects.ez.no/admin2pp">admin2++</a> extension',
-                      'Version'   => '0.1alpha1',
+                      'Version'   => '0.2alpha1',
                       'Copyright' => 'Copyright (C) 2010 Damien Pobel',
                       'License'   => 'GNU General Public License v2.0',
                       'Includes the following library' => array( 'Name'      => 'jQueryUI',
-                                                                 'Version'   => '1.8.0',
+                                                                 'Version'   => '1.8.6',
                                                                  'Copyright' => '<a href="http://jqueryui.com/about">jQuery UI Team</a>',
                                                                  'License'   => 'GNU General Public License v2.0',
                                                                ),
